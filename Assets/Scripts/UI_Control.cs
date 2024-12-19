e91a908f6496d3f86406cd795e86c5aa581b697b
Khine Zin Zin Linn <khinezinzinlinn@gmail.com>
2024-12-19 22:35:02 +0700

    .....

--- Assets/Scripts/UI_Control.cs
+++ Assets/Scripts/UI_Control.cs
@@ -7,14 +7,13 @@ public class UI_Control : MonoBehaviour
 {
     public GameObject menuCanvas;
     public GameObject controlsCanvas;
-    public GameObject dialogueCanvas;
-    public GameObject dialogueText;
+    public string storyline1;
+    public string storyline2;
 
     public void PlayButton()
     {
-        
-        dialogueCanvas.SetActive(true);
-        //SceneManager.LoadScene("Game");
+        menuCanvas.SetActive(false);
+        Dialogue_Control.instance.DialogueCanvas_Activate(storyline1);        
     }
 
     public void ControlButton()
@@ -32,6 +31,14 @@ public class UI_Control : MonoBehaviour
     public void QuitButton()
     {
         Application.Quit();
+    }
+
+    public void SkipButton()
+    {
+        Dialogue_Control.instance.DialogueCanvas_Deactivate();
+        Dialogue_Control.instance.DialogueCanvas_Activate(storyline2);
+        //Dialogue_Control.instance.DialogueCanvas_Deactivate();
+        //SceneManager.LoadScene("Game");
     }
 
 }
